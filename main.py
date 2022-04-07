c8b7de9a12a0bdf46ed1fe49973484773e2d3dc4
Mattia Matteini <matteinimattia@gmail.com>
2022-04-07 13:30:57 +0200

    fixed priorities for objective function, fixed overlapping constraints

--- main.py
+++ main.py
@@ -23,38 +23,44 @@ constants = json.loads(constansts_file.read())
 paws = json.loads(paws_file.read())
 dlos = json.loads(dlos_file.read())
 
+
 def overlap(event1, event2):
     return event1['start_time'] <= event2['stop_time'] and event1['stop_time'] >= event2['start_time']
+
 
 # get rid of dtos overlapping with paws and dlos
 filtered_dtos = []
 for dto in dtos:
     skip = False
     for event in paws + dlos:
-        if (overlap(dto, event)):
+        if overlap(dto, event):
             skip = True
-            #print("qua")
             break
     if not skip:
         filtered_dtos.append(dto)
 
-print(len(dtos))
-print(len(filtered_dtos))
+print(f"Total DTOs: {len(dtos)}")
+print(f"Filtered DTOs: {len(filtered_dtos)}")
 dtos = filtered_dtos
 
 # Number of DTOs overlapping with the first one
-count = 0
-for dto in dtos:
-    if (overlap(dto, dtos[0])):
-        count = count + 1
-print(count)
+# count = 0
+# for dto in dtos:
+#     if overlap(dto, dtos[0]):
+#         count = count + 1
+# print(count)
 
 CAPACITY = constants['MEMORY_CAP']
 DTOS_NUMBER = len(dtos)
-incidences = np.array(list(map(lambda dto: dto["incidence"], dtos)))
+
+priorities = []
+
+# populate array of priorities
+for index_dto, dto in enumerate(dtos):
+    priorities.append(next((ar['rank'] for ar in ars if ar['id'] == dto['ar_id']), None))
+
 memories = np.array(list(map(lambda dto: dto["memory"], dtos)))
 dtos_id = np.array(list(map(lambda dto: dto["id"], dtos)))
-
 
 # Read and solve model
 model = gp.Model()
@@ -65,53 +71,22 @@ start = time.time()
 # add the decision variable to the model
 variables = model.addMVar(DTOS_NUMBER, vtype=GRB.BINARY)
 
-variables_constraint = []
-
-# print(np.ufunc.reduce(np.array(dtos), dtype=None, out=None, keepdims=False, initial={'start_time': 0, 'stop_time': 0},
-#                      where=lambda acc, dto: dto))
-
-
-from functools import reduce
-
-
-# def get_start_time(a):
-#     return a['start_time']
-#
-#
-# vfunc = np.vectorize(get_start_time)
-#
-# for dto in dtos:
-#     indexes = np.where(np.logical_and(dto['start_time'] <= vfunc(dtos), vfunc(dtos) <= dto['stop_time']))
-#     print(indexes[0])
-#     break
-
+# for each couple of dtos which overlap add a constraint
 for i1, dto1 in enumerate(dtos):
     dto1_overlap = []
     for i2, dto2 in enumerate(dtos):
-        if (overlap(dto1, dto2)):
+        if overlap(dto1, dto2) and dto1 != dto2:
             dto1_overlap.append(i2)
-    print("Overlapping with dto " + str(i1) + ": " + str(len(dto1_overlap)))
-
-
-
-for dto1 in dtos:
-    for dto2 in dtos:
-        if dto1['start_time'] <= dto2['stop_time'] and dto1['stop_time'] >= dto2['start_time']:
-        #if dto2['start_time'] <= dto1['start_time'] <= dto2['stop_time']:
-            variables_constraint.append(variables[dtos.index(dto1)])
-            variables_constraint.append(variables[dtos.index(dto2)])
-            print([dtos.index(dto1), dtos.index(dto2)])
             # add overlapping contraints
-            model.addConstr(gp.quicksum(variables_constraint) <= 1, "Overlapping constraint" + str(dtos.index(dto1)))
-            #variables_constraint = []
-    print(len(variables_constraint))
-    break
+            model.addConstr(variables[i1] + variables[i2] <= 1, "Overlapping constraint" + str(dtos.index(dto1)))
+
+    print("Overlapping with dto " + str(i1) + ": " + str(len(dto1_overlap)))
 
 
 # add the memory constraint
 model.addConstr(gp.quicksum([memories[i] * variables[i] for i in range(DTOS_NUMBER)]) <= CAPACITY, "Memory constraint")
-# set objective function to maximize dto's incidences
-model.setObjective(gp.quicksum([incidences[i] * variables[i] for i in range(DTOS_NUMBER)]), GRB.MAXIMIZE)
+# set objective function to maximize dto's priority
+model.setObjective(gp.quicksum([priorities[i] * variables[i] for i in range(DTOS_NUMBER)]), GRB.MAXIMIZE)
 
 end = time.time()
 print("Preparation terminated in ", end - start)
