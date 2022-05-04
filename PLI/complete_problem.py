9697784ae7a6b674a4fc6724c76a13c5a3347250
Mattia Matteini <matteinimattia@gmail.com>
2022-05-04 18:46:18 +0200

    added dtos remained in memory

--- PLI/complete_problem.py
+++ PLI/complete_problem.py
@@ -1,23 +1,21 @@
 import sys
-import json
-
 import numpy as np
 import json
 import gurobipy as gp
 from gurobipy import GRB
 import time
 from utils.overlap import overlap
-import pandas as pd
 import matplotlib.pyplot as plt
-import re
+
+INSTANCE = 'day1_40'
 
 # read JSON files
 
-dtos_file = open('../data/day1_0/DTOs.json')
-ars_file = open('../data/day1_0/ARs.json')
-constants_file = open('../data/day1_0/constants.json')
-paws_file = open('../data/day1_0/PAWs.json')
-dlos_file = open('../data/day1_0/DLOs.json')
+dtos_file = open(f'../data/{INSTANCE}/DTOs.json')
+ars_file = open(f'../data/{INSTANCE}/ARs.json')
+constants_file = open(f'../data/{INSTANCE}/constants.json')
+paws_file = open(f'../data/{INSTANCE}/PAWs.json')
+dlos_file = open(f'../data/{INSTANCE}/DLOs.json')
 
 # loads JSON, the result is a dictionary
 dtos = json.loads(dtos_file.read())
@@ -78,7 +76,7 @@ dtos_variables = list(model.addMVar((DTOS_NUMBER,), vtype=GRB.BINARY, name="DTOs
 
 x_ji = []
 for index in range(DLOS_NUMBER):
-    x_ji.append(list(model.addMVar((DTOS_NUMBER,), vtype=GRB.BINARY, name=f"DTOs downloaded in DLO {index}")))
+    x_ji.append(list(model.addMVar((DTOS_NUMBER,), vtype=GRB.BINARY, name=f"DTOs_downloaded_in_DLO_{index}")))
 
 grouped_dtos = dict()
 
@@ -86,9 +84,9 @@ grouped_dtos = dict()
 for i1, dto1 in enumerate(dtos):
     for i2, dto2 in enumerate(dtos):
         if overlap(dto1, dto2) and dto1 != dto2:
-            # add overlapping contraints
+            # add overlapping constraints
             model.addLConstr(dtos_variables[i1] + dtos_variables[i2] <= 1,
-                             f"Overlapping constraint for DTOs {dto1['id']} and {dto2['id']}")
+                             f"Overlapping_constraint_for_DTOs_{dto1['id']}_and_{dto2['id']}")
 
     if dto1['ar_id'] not in grouped_dtos.keys():
         grouped_dtos[dto1['ar_id']] = [dto1]
@@ -98,12 +96,12 @@ for i1, dto1 in enumerate(dtos):
 # add the single satisfaction constraints
 for ar_id in grouped_dtos.keys():
     model.addLConstr(gp.quicksum([dtos_variables[dtos.index(dto_)] for dto_ in grouped_dtos[ar_id]]) <= 1,
-                     f"Single satisfaction constraint for AR {ar_id}")
+                     f"Single_satisfaction_constraint_for_AR_{ar_id}")
 
 # add the taken memory constraints
 satellite_memories = [gp.quicksum([memories[i] * dtos_variables[i] for i, dto in enumerate(dtos)
                                    if dto['stop_time'] < dlos[0]['start_time']])]
-model.addLConstr(satellite_memories[0] <= CAPACITY, f'Memory constraint DLO 0')
+model.addLConstr(satellite_memories[0] <= CAPACITY, f'Memory_constraint_DLO_0')
 
 for j in range(1, DLOS_NUMBER):
     satellite_memories.append(satellite_memories[j - 1] -
@@ -112,37 +110,32 @@ for j in range(1, DLOS_NUMBER):
                                            if dto['start_time'] > dlos[j - 1]['stop_time']
                                            and dto['stop_time'] < dlos[j]['start_time']]))
 
-    model.addLConstr(satellite_memories[j] <= CAPACITY, f'Memory constraint DLO {j}')
+    model.addLConstr(satellite_memories[j] <= CAPACITY, f'Memory_constraint_DLO_{j}')
 
-# add constraint for downlink DTO, it must be chosen in the plan
-for j in range(DLOS_NUMBER):
-    for i in range(DTOS_NUMBER):
-        model.addLConstr(x_ji[j][i] <= dtos_variables[i], f'Downlink only if DTO is taken DLO:{j}, DTO:{i}')
-
-# add single downlink constraint
-for i in range(DTOS_NUMBER):
-    model.addLConstr(gp.quicksum([x_ji[j][i] for j in range(DLOS_NUMBER)]) <= 1)
+# # add DTO selected in plan constraint
+# for j in range(DLOS_NUMBER):
+#     for i in range(DTOS_NUMBER):
+#         model.addLConstr(x_ji[j][i] <= dtos_variables[i], f'DTO_selected_in_plan_constraint_DLO:{j}_DTO:{i}')
+#
+# # add single downlink constraint
+# for i in range(DTOS_NUMBER):
+#     model.addLConstr(gp.quicksum([x_ji[j][i] for j in range(DLOS_NUMBER)]) <= 1)
 
 # last two constraints can be reduced to the next one (maybe)
-# for i in range(DTOS_NUMBER):
-#     model.addLConstr(gp.quicksum([x_ji[j][i] for j in range(DLOS_NUMBER)]) <= dtos_variables[i])
+for i in range(DTOS_NUMBER):
+    model.addLConstr(gp.quicksum([x_ji[j][i] for j in range(DLOS_NUMBER)]) <= dtos_variables[i])
 
 # add downloaded memory constraint
 for j in range(DLOS_NUMBER):
-    if j == 15:
-        print(j)
-    print(dlos[j]['stop_time'], " - ", dlos[j]['start_time'])
-    print(dlos[j]['stop_time'] - dlos[j]['start_time'])
-    print(DOWNLINK_RATE * (dlos[j]['stop_time'] - dlos[j]['start_time']))
-
     model.addLConstr(gp.quicksum([memories[i] * x_ji[j][i] for i in range(DTOS_NUMBER)]) <=
-                     DOWNLINK_RATE * (dlos[j]['stop_time'] - dlos[j]['start_time']), f'Downloaded memory constraint')
+                     DOWNLINK_RATE * (dlos[j]['stop_time'] - dlos[j]['start_time']),
+                     f'Downloaded_memory_constraint_DLO_{j}')
 
 # add time constraint
 for j in range(DLOS_NUMBER):
     for i in range(DTOS_NUMBER):
         if dlos[j]['start_time'] < dtos[i]['stop_time']:
-            model.addLConstr(x_ji[j][i] == 0, f'Time constraint for DTO:{i}')
+            model.addLConstr(x_ji[j][i] == 0, f'Time_constraint_for_DTO_{i}_DLO_{j}')
 
 # set objective function to maximize dto's priority
 model.setObjective(gp.quicksum([priorities[i] * dtos_variables[i] for i in range(DTOS_NUMBER)]), GRB.MAXIMIZE)
@@ -168,10 +161,27 @@ if model.Status == GRB.OPTIMAL:
     print(json_solution)
 
     # take the DTOs in the plan
-    sol_dtos = [dtos[index] for index in range(DTOS_NUMBER) if dtos_variables[index].getAttr("X") == 1]
-    print(sol_dtos)
-    print(x_ji[0][0])
+    dtos_taken = [dtos[index] for index in range(DTOS_NUMBER) if dtos_variables[index].getAttr("X") == 1]
 
+    # calculate which dtos are downloaded
+    dtos_in_memory = []
+    dtos_downloaded = []
+    for i in range(DTOS_NUMBER):
+        downloaded = False
+        j = 0
+        while j < DLOS_NUMBER and not downloaded:
+            if x_ji[j][i].getAttr("X") == 1:
+                downloaded = True
+                dtos_downloaded.append(dtos[i])
+            j = j + 1
+
+    # dtos remained in memory at the end of plan
+    dtos_in_memory = [dto for dto in dtos_taken if dto not in dtos_downloaded]
+    memories_in_memory = list(map(lambda dto_: dto_['memory'], dtos_in_memory))
+    print("Memoria occupata:", sum(memories_in_memory))
+    print("DTOs rimaste in memoria", dtos_in_memory)
+
+    # memories freed during the plan
     freed_memories = []
     for j in range(DLOS_NUMBER):
         freed_memory = 0
@@ -181,11 +191,9 @@ if model.Status == GRB.OPTIMAL:
 
         freed_memories.append(freed_memory)
 
-    print(freed_memories)
-
-    activities = sol_dtos + dlos
+    # calculate memories for each activity to plot the memory graph
+    activities = dtos_taken + dlos
     activities = sorted(activities, key=lambda activity_: activity_['start_time'])
-
     tot_memory = 0
     chronology_memories = []
     for activity in activities:
@@ -196,14 +204,19 @@ if model.Status == GRB.OPTIMAL:
 
         chronology_memories.append(tot_memory)
 
-    print(chronology_memories)
     xx = np.arange(len(chronology_memories))
     plt.plot(xx, chronology_memories)
     plt.title('Memory')
     plt.show()
 
-    with open('../data/day1_0/result.json', 'w') as f:
+    with open(f'../data/{INSTANCE}/result.json', 'w') as f:
         json.dump(json_solution, f)
+
+    # with open(f'../data/{INSTANCE}/result.json') as f:
+    #     old_json = json.loads(f.read())
+    #     print(old_json['Vars'])
+    #     print(json_solution['Vars'])
+    #     print(old_json['Vars'] == json_solution['Vars'])
 
     model.write('model.lp')
 
